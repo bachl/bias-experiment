afad69cc6a2eb09c78bfd54bf5560320b8da9bb1
Wouter van Atteveldt <wouter@vanatteveldt.com>
2024-07-16 21:22:03 +0200

    Improve sentence display, faceting

--- src/analysis/exploratie.R
+++ src/analysis/exploratie.R
@@ -2,57 +2,46 @@ d |> group_by(country, round, specified, sentence, stance) |>
   summarize(PT6=mean(PT6, na.rm=T)) |>
   ggplot(aes(x=stance, y=sentence, label=round(PT6, 1), fill=PT6)) + geom_tile() + geom_text()
 
+actors = d |> select(sentence, actor)  |> unique() |> filter(actor != "X")
 
 
 d2 <- d |> 
   mutate(mask=if_else(actor == "X", "masked", "notmasked")) |>
   filter(!is.na(stance), stance != "dontknow") |>
   group_by(country, round, specified, sentence, gold, mask, stance) |>
-  summarize(PT6=mean(PT6, na.rm=T), n=n()) |>
+  summarize(m=mean(PT6, na.rm=T), n=n(), sd=sd(PT6, na.rm=T)) |>
+  inner_join(actors) |>
   mutate(p=n/sum(n)) |>
-  mutate(sentence=glue::glue("[{country}{round} {gold}] {sentence}")) 
-  #pivot_wider(names_from="stance", values_from=PT6) |>
-ggplot(d2, aes(y=sentence, x=PT6)) + 
+  mutate(sentence=if_else(!str_detect(sentence, "Euro"), sentence,
+                          "zegt dat [de EU] slecht geweest voor Nederland is."
+                          )) |>
+  mutate(sentence=glue::glue("[{actor}/X] {sentence} ")) |>
+  mutate(sentence = fct_reorder(sentence, round))
+  
+
+ggplot(d2, aes(y=sentence, x=m)) + 
   geom_point(aes(size=p, color=stance), alpha=.6) +
-  geom_text(data=filter(d2, p>.43), aes(label=round(p*100, 0)), size=3)+
-  facet_grid(rows=vars(specified), cols=vars(mask), scales = "free_y") + 
+  geom_text(data=filter(d2, stance==gold), aes(label=round(p*100, 0)), size=3)+
+  ggh4x::facet_nested(specified + country ~ mask , scales="free_y")+ 
   theme_minimal() + 
-  theme(legend.position="none") + 
-  xlab("Self-reported Left-Right")
+  scale_size(guide="none") + 
+  theme(legend.position="bottom") + 
+  xlab("Self-reported Left-Right") + 
+  ylab("") +
+  theme(legend.title = element_blank(), 
+        strip.background = element_rect(color = "black", size = .5))
 
-
-  geom_segment(aes(x=pro, xend=anti)) +
-  geom_point(aes(x=pro, size=n), color="blue") +
-  geom_point(aes(x=anti, size=n), color="red") +
-  geom_point(aes(x=neutral, size=n), color="orange") +
-  facet_grid(rows=vars(specified), scales = "free") + 
+# With +/- 1 SD 'error' bars
+ggplot(d2, aes(y=sentence, x=m)) + 
+  geom_segment(aes(x=m-sd, xend=m+sd, color=stance), position = position_dodge(width=.5)) + 
+  geom_point(aes(size=p, color=stance), alpha=.6, position = position_dodge(width=.5)) +
+  geom_text(data=filter(d2, stance=="pro", gold=="pro"), aes(label=round(p*100, 0)), size=3, nudge_y = .15)+
+  geom_text(data=filter(d2, stance=="anti", gold=="anti"), aes(label=round(p*100, 0)), size=3, nudge_y = -.15)+
+  ggh4x::facet_nested(specified + country ~ mask , scales="free_y")+ 
   theme_minimal() + 
-  theme(legend.position="none") + 
-  xlab("Self-reported Left-Right")
-
-d |>
-  mutate(sentence=glue::glue("[{country}{round} {gold}] {sentence}")) |>
-  filter(!is.na(stance), stance != "dontknow") |>
-  ggplot() + 
-  geom_jitter(aes(x=PT6, y=sentence, color=stance), alpha=.25, width=.1) +
-  theme_minimal() 
-  
-
-  ggridges::geom_density_ridges(aes(x=PT6, y=sentence, fill=stance), alpha=.5, bandwidth=.15) +
-  facet_grid(rows=vars(specified),scales = "free") 
-  #facet_grid(rows=vars(specified), cols=vars(stance), scales = "free") 
-
-
-d |> 
-  filter(!is.na(stance), stance != "dontknow") |>
-  mutate(stance=if_else(stance == "neutral", "neutral", "stance")) |>
-  group_by(country, round, specified, sentence, stance) |>
-  summarize(PT6=mean(PT6, na.rm=T)) |>
-  pivot_wider(names_from="stance", values_from=PT6) |>
-  ggplot(aes(y=sentence, yend=sentence)) + 
-  geom_segment(aes(x=neutral, xend=stance)) +
-  geom_point(aes(x=neutral), color="blue") +
-  geom_point(aes(x=stance), color="red") 
-
-  
-  ggplot(aes(x=stance, y=sentence, label=round(PT6, 1), fill=PT6)) + geom_tile() + geom_text()
+  scale_size(guide="none") + 
+  theme(legend.position="bottom") + 
+  xlab("Self-reported Left-Right") + 
+  ylab("") +
+  theme(legend.title = element_blank(), 
+        strip.background = element_rect(color = "black", size = .5))
