ac9fecf994af20443aa51291c5b3f3f86a7c9145
Marko <marko.bachl@gmail.com>
2025-09-12 11:56:46 +0200

    Checked
    and everything seems fine now

--- src/lib/functions.R
+++ src/lib/functions.R
@@ -14,5 +14,5 @@ fig_cols <- yarrr::piratepal(palette = "basel",
                              trans = .2)
 fig_cols <- as.character(fig_cols[1:9])
 
-api_key_fn <- here("data/raw-private/qualtrics_api_key.txt")
-API <- read_file(api_key_fn) %>% trimws()
+# api_key_fn <- here("data/raw-private/qualtrics_api_key.txt")
+# API <- read_file(api_key_fn) %>% trimws()
